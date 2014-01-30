590a680dba13f32078ec09b361fb9546fb5e547c
ywang135 <ywang135@binghamton.edu>
2014-01-30 13:18:22 -0500

    add header, footer and css file

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>无标题文档</title>
+</head>
+
+<body>
+<pre>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.01//EN&quot;
+          &quot;http://www.w3.org/TR/html4/strict.dtd&quot;&gt;
+&lt;html lang=&quot;en&quot;&gt;
+	&lt;head&gt;
+		&lt;meta http-equiv=&quot;content-type&quot; content=&quot;text/html; charset=utf-8&quot;&gt;
+		&lt;link rel=&quot;stylesheet&quot; href=&quot;css/resolution.css&quot; type=&quot;text/css&quot; media=&quot;screen&quot;&gt;
+		&lt;title&gt;Loading...&lt;/title&gt;
+	&lt;/head&gt;
+&lt;body&gt;</pre>
+</body>
+</html>
