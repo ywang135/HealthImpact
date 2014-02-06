523e0a0d0553057272231d621430550bf3fa453f
Yi Wang <yige@Yi.local>
2014-02-06 15:15:51 -0500

    add 4 squares to index page

--- index.php
+++ index.php
@@ -8,7 +8,16 @@
        	$("#sub_menu").show();
     });
 </script>
-
+	<div class="main_body">
+		<div id="line_1">
+		<div id="sq_l_t"></div>
+		<div id="sq_r_t"></div>
+		</div><!-- #line_1 end -->
+		<div id="line_2">
+		<div id="sq_l_b"></div>
+		<div id="sq_r_b"></div>
+		</div><!-- #line_2 end -->
+	</div><!-- .main_body end -->
 
 <?php
 	include "footer.php";
