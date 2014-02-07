5129c7ea867811b3a77397489bfeeac5a54f5dc2
Yi Wang <yige@Yi.local>
2014-02-06 21:19:11 -0500

    add country.php (empty file)

---
+++ country.php
@@ -0,0 +1,18 @@
+<?php
+	include "header.php";
+?>
+<script type="text/javascript">
+	$title = "The Global Health Impact";
+    $(document).ready(function() {
+        $(this).attr("title", $title);
+       	$("#sub_menu").show();
+       	$("#copyright").css( { "margin-top" : "700px"} );
+    });
+</script>
+	<div class="main_body">
+
+	</div><!-- .main_body end -->
+
+<?php
+	include "footer.php";
+?>
