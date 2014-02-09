b46247720e9be57f34da5aad8b6aebf8e9eeb92d
Yi Wang <yige@Yi.local>
2014-02-09 17:55:44 -0500

    add map data

--- JS/jq.js
+++ JS/jq.js
@@ -58,44 +58,92 @@ function myJSAll(vName, sc1, sc2, sc3, tsc, iso_code){
 	var str = "<br>Overall Impact: "+tsc+"<br>ISO code: "+iso_code;
 	document.getElementById('additional_statistic').innerHTML=vName;			
 	document.getElementById('country_iso').innerHTML=str;
-	drawChart("bar_details", vName, parseFloat(sc1), parseFloat(sc2), parseFloat(sc3));
+	drawChart("bar_details", vName, parseFloat(sc1), parseFloat(sc2), parseFloat(sc3),0);
 }
 function myJSMalaria(vName, sc1, sc2, sc3, tsc, iso_code){
 	var str = "<br>Overall Impact: "+tsc+"<br>ISO code: "+iso_code;
 	document.getElementById('additional_statistic').innerHTML=vName;			
 	document.getElementById('country_iso').innerHTML=str;
-	drawChart("bar_details", vName, parseFloat(sc1), parseFloat(sc2), parseFloat(sc3));
+	drawChart("bar_details", vName, parseFloat(sc1), parseFloat(sc2), parseFloat(sc3),1);
 }
 function myJSTB(vName, sc1, sc2, sc3, tsc, iso_code){
 	var str = "<br>Overall Impact: "+tsc+"<br>ISO code: "+iso_code;
 	document.getElementById('additional_statistic').innerHTML=vName;			
 	document.getElementById('country_iso').innerHTML=str;
-	drawChart("bar_details", vName, parseFloat(sc1), parseFloat(sc2), parseFloat(sc3));
+	drawChart("bar_details", vName, parseFloat(sc1), parseFloat(sc2), parseFloat(sc3),2);
 }
 function myJSHIV(vName, sc1, sc2, sc3, tsc, iso_code){
 	var str = "<br>Overall Impact: "+tsc+"<br>ISO code: "+iso_code;
 	document.getElementById('additional_statistic').innerHTML=vName;			
 	document.getElementById('country_iso').innerHTML=str;
-	drawChart("bar_details", vName, parseFloat(sc1), parseFloat(sc2), parseFloat(sc3));
+	drawChart("bar_details", vName, parseFloat(sc1), parseFloat(sc2), parseFloat(sc3),3);
 }
 google.load("visualization", "1", {packages: ["corechart"]});
 google.setOnLoadCallback(drawChart);
-function drawChart(div_name, countryName, imp1,imp2,imp3) {
-	var data = google.visualization.arrayToDataTable([
+function drawChart(div_name, countryName, imp1,imp2,imp3,type) {
+	if(type==0){
+		var data = google.visualization.arrayToDataTable([
         	['Impact Name', 'Score', { role: 'style' }],
         	['Total Malaria Treatment Impact', imp1, 'color: #0083CA' ],
         	['Total TB Treatment Impact', imp2, 'color: #FFB31C' ],
         	['Total HIV Treatment Impact', imp3, 'color: #EF3E2E' ]
      	 	]);
-	var options = {
-		title: countryName,
-    	width: 337,
-        height: 180,
-        marginTop: 100,
-        legend: { position: "none" },//'top', maxLines: 1
-		bar: { groupWidth: '50%' },
-        isStacked: true,
-    };
+     	var options = {
+			title: countryName,
+    		width: 337,
+        	height: 180,
+        	marginTop: 100,
+        	legend: { position: "none" },//'top', maxLines: 1
+			bar: { groupWidth: '50%' },
+        	isStacked: true,
+    	};
+    }
+    else if(type==1){
+		var data = google.visualization.arrayToDataTable([
+        	['Impact Name', 'Score', { role: 'style' }],
+        	['Total Malaria Treatment Impact', imp1, 'color: #0083CA' ]
+     	 	]);     	
+     	var options = {
+			title: countryName,
+    		width: 337,
+        	height: 180,
+        	marginTop: 100,
+        	legend: { position: "none" },//'top', maxLines: 1
+			bar: { groupWidth: '20%' },
+        	isStacked: true,
+    	};
+    }
+    else if(type==2){
+		var data = google.visualization.arrayToDataTable([
+        	['Impact Name', 'Score', { role: 'style' }],
+        	['Total TB Treatment Impact', imp2, 'color: #FFB31C' ]
+     	 	]);
+     	var options = {
+			title: countryName,
+    		width: 337,
+        	height: 180,
+        	marginTop: 100,
+        	legend: { position: "none" },//'top', maxLines: 1
+			bar: { groupWidth: '20%' },
+        	isStacked: true,
+    	};
+    }
+    else if(type==3){
+		var data = google.visualization.arrayToDataTable([
+        	['Impact Name', 'Score', { role: 'style' }],
+        	['Total HIV Treatment Impact', imp3, 'color: #EF3E2E' ]
+     	 	]);
+     	var options = {
+			title: countryName,
+    		width: 337,
+        	height: 180,
+        	marginTop: 100,
+        	legend: { position: "none" },//'top', maxLines: 1
+			bar: { groupWidth: '20%' },
+        	isStacked: true,
+    	};
+    }
+
 
     var chart = new google.visualization.BarChart(document.getElementById(div_name));
     chart.draw(data, options);
