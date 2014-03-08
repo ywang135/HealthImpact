951ac1eaf71b4a357c5bb5bb0f87f6fc39774c0d
Yi Wang <yige@Yi.local>
2014-03-07 23:54:27 -0500

    modify text and picture in m.. page

--- methodology.php
+++ methodology.php
@@ -7,27 +7,23 @@
 		<div id="left_bar">
 		<subtitle>The Drug Index</subtitle>
 		<p>To evaluate the impact of the major drugs for malaria, tuberculosis and HIV/AIDS, 
-		we consider three things:
-		<ul>
-		<li>The need for the drugs.</li>
-		<li>The drugs’ effectiveness.</li>
-		<li>The number of people who can access the drugs.</li></ul>
-		</p>
-		<p>Drug scores are, roughly, Need*Access*Efficacy</p>
+		we consider 1) the need for the drugs, 2) the drugs’ effectiveness, and 3) access 
+		to the drug. Very roughly, we multiply the need for the drug - measured in Disability 
+		Adjusted Life-Years (DALYS) lost to the diseases they treat -- times drug efficacy and 
+		treatment percentages to get the final impact scores. </p>
 
 		<subtitle>The Country Index</subtitle>
-		<p>The Country Index sums all drugs’ impacts within the country. 
-		So, for instance, if two drugs are used in some country and the 
-		first averts 1 million DALYS and the second averts 2 million. 
-		The country score would be 3 million DALYS averted.</p>
-		<p>We also break down the scores so that it is possible to see the DALYS averted 
-		by drugs for malaria, TB, and HIV/AIDS respectively. </p>
+		<p>The Country Index sums all drugs’ impacts within the country. So, for instance, 
+		if two drugs are used in some country and the first averts 1 million DALYS and the 
+		second averts 2 million. The country score would be 3 million DALYS averted. We 
+		also break down the scores so that it is possible to see the DALYS averted by drugs 
+		for malaria, TB, and HIV/AIDS respectively. </p>
 
 		<subtitle>The Company Index</subtitle>
 		<p>The Company Index sums the impact of all of a company’s drugs. So, for instance, 
 		if one company makes two drugs that avert 3 and 6 million DALYS respectively, 
-		the Company Score would be 9 million DALYS averted.</p>
-		<p>We also break down the scores so that it is possible to see the DALYS averted 
+		the Company Score would be 9 million DALYS averted.We also break down the scores so 
+		that it is possible to see the DALYS averted 
 		by company for malaria, TB, and HIV/AIDS respectively.</p>
 
 		<subtitle>The Disease Index</subtitle>
@@ -41,7 +37,7 @@
 			<table cellpadding="5">
 				<tr><td><div><a href="http://academicsstand.org/wp-content/uploads/2013/03/WP_11_2012_Hassoun.pdf">
 				<img src="images/WP_11_2012_Hassoun.jpg" width="85px" height="130px"/></a></div></td>
-				<td><subtitle>The Global Health Impact Index: Promoting Global Health</><br>
+				<td><subtitle>The Global Health Impact Index</subtitle><br>
 				<p>
 				 This report explains in detail how the Company Index is calculated. In doing so, 
 				 it gives examples of how drug scores are calculated.
