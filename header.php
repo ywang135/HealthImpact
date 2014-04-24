0842f0e6dcb1856e40dd0fc36ae18c85f48aabe8
Yi Wang <yige@Yi.local>
2014-04-24 14:14:31 -0400

    change the order of icons

--- header.php
+++ header.php
@@ -17,11 +17,11 @@
 			<form method="get" action="http://www.google.com/search">
 			<table border="0" cellpadding="0">
   				<tr><td>
-  				<input type="text" name="q" size="22" maxlength="255" title="search" value="" placeholder="Search" style=""/>
+  				<input type="text" name="q" size="22" maxlength="255" title="search" value="" placeholder="Search" style="margin-top:5px;"/>
   				<input type="hidden" name="sitesearch" value="www.binghamton.edu"/>
   				</td><td>
   				<input type="image" name="sa" id="edit-sa" alt="Search" class="form-submit form-submit search-submit" 
-  				src="images/1391730788_84062.ico" title="search" width="30" height="30" >
+  				src="images/1391730788_84062.ico" title="search" width="30" height="30" style="margin-top:2px">
   				</td></tr>
   			</table>
   			</form>
@@ -31,9 +31,9 @@
   				<tr>
   				<td height="33"><a href="https://www.facebook.com/AcademicsStandAgainstPoverty" target="_blank"><img src="images/facebook.ico" alt="Join Our Facebook Fan Page" width="32" height="32" border="0"></a>
 				</td>
-				<td height="33"><a href="" target="_blank"><img src="images/twitter.ico" alt="Follow us twitter Page" width="32" height="32" border="0"></a>
+				<td height="33"><a href="" target="_blank"><img src="images/linkedin.ico" alt="Join Our LinkedIn Page" width="32" height="32" border="0"></a>
 				</td>
-  				<td height="33"><a href="" target="_blank"><img src="images/linkedin.ico" alt="Join Our LinkedIn Page" width="32" height="32" border="0"></a>
+				<td height="33"><a href="" target="_blank"><img src="images/twitter.ico" alt="Follow us twitter Page" width="32" height="32" border="0"></a>
 				</td></tr>
   			</table>
 			</div><!-- #network end -->
