670ba40d9dd5abb59b27ddf02dfd851b86b6bcaa
Yi Wang <yige@Yi.local>
2014-04-26 14:04:11 -0400

    add counter

--- footer.php
+++ footer.php
@@ -1,4 +1,21 @@
 		<div id="copyright"><p>&copy;2014 The Global Health Impact</p>
+		<!-- Start of StatCounter Code for Default Guide -->
+<script type="text/javascript">
+var sc_project=9768318; 
+var sc_invisible=0; 
+var sc_security="0f8737fb"; 
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<noscript><div class="statcounter"><a title="hit counter"
+href="http://statcounter.com/" target="_blank"><img
+class="statcounter"
+src="http://c.statcounter.com/9768318/0/0f8737fb/0/"
+alt="hit counter"></a></div></noscript>
+<!-- End of StatCounter Code for Default Guide -->
 		</div>
 	</container>
   </body>
